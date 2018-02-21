46a66c6f234ead138f665b6d528953fd69f7f1f5
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2018-02-21 17:05:30 +0000

    save load

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,7 @@ var labels = '123456789';
 var labelIndex = 0;
 var directionsDisplay;
 var directionsService;
+var points=[];
 
 //when the jQuery Mobile page is initialised
 $(document).on('pageinit', function() {
@@ -15,43 +16,19 @@ $(document).on('pageinit', function() {
 	//change time box to show message
 	$('#time').val("Press the button to get location data");
     
+    /*
     var locationOptions = {
-        //maximumAge: 10000,
-        //timeout: 6000,
-        //enableHighAccuracy: true
+        maximumAge: 10000,
+        timeout: 6000,
+        enableHighAccuracy: true
     };
 
     //navigator.geolocation.watchPosition(updatePosition,failPosition,locationOptions);
     //navigator.geolocation.getCurrentPosition(updatePosition, failPosition);
+    */
     //Center Map
     centerMap();
 });
-
-function addCheck(){
-    var center = map.getCenter();
-    addMarker(center);
-}
-
-function addMarker(location){
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map,
-        label: labels[labelIndex++ % labels.length],
-        draggable: true,
-        
-    });
-    markers.push(marker);
-
-    console.log("add marker");
-}
-
-function deleteMarkers(){
-    for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-    }
-    markers=[];
-    labelIndex=0;
-}
 
 function centerMap(){
     console.log("center map");
@@ -65,7 +42,17 @@ function centerPosition(position){
     //Load map
     initMap();
     //Add Start Finish Markers
-    startFinishMarkers();
+    //startFinishMarkers();
+    route ={
+          origin: {lat: currentPos.lat,lng: currentPos.lng-0.003},  // Haight.
+          destination: {lat: currentPos.lat,lng: currentPos.lng+0.003},  // Ocean Beach.
+          // Note that Javascript allows us to access the constant
+          // using square brackets and a string value as its
+          // "property."
+          waypoints: markers,
+          travelMode: google.maps.TravelMode['WALKING']
+    };
+    calcRoute(directionsService, directionsDisplay, route);
 }
 
 //called when the position is successfully determined
@@ -88,47 +75,21 @@ function failPosition(error) {
 
 function initMap(){
     detectBrowser();
-    directionsDisplay = new google.maps.DirectionsRenderer;
-    directionsService = new google.maps.DirectionsService;
+
      map = new google.maps.Map(document.getElementById('map'), {
         center: currentPos,
         zoom: 15
     });
-    directionsDisplay.setMap(map);
     
-}
-
-function startFinishMarkers(){
-    
-    //Start Marker
-    var start = new google.maps.Marker({
-        position: {lat: currentPos.lat,lng: currentPos.lng-0.003},
-        map: map,
-        label:{
-            text:'START',
-            fontWeight: '900'
-        },
-        animation: google.maps.Animation.DROP,
-        draggable: true
-    });
-
-    
-    //Finish Marker
-    var finish = new google.maps.Marker({
-        position: {lat: currentPos.lat,lng: currentPos.lng+0.003},
-        map: map,
-        label:{
-            text:'FINISH',
-            fontWeight: '900',
-        },
-        animation: google.maps.Animation.DROP,
-        draggable: true
+    directionsDisplay = new google.maps.DirectionsRenderer({
+        draggable:true,
+        map: map
     });
     
-    //Add markers
-    markers.push(start);
-    markers.push(finish);
+    directionsService = new google.maps.DirectionsService;
+
 }
+
 
 function detectBrowser() {
     var useragent = navigator.userAgent;
@@ -137,32 +98,47 @@ function detectBrowser() {
     mapdiv.style.height = '500px';
 }
 
-function save(){
-;
-    var start = markers[0].getPosition();
-    var fin = markers[1].getPosition();
-    
-    $('#slongtext').val(start.lng());
-    $('#slattext').val(start.lat());
-    $('#flongtext').val(fin.lng());
-    $('#flattext').val(fin.lat());
-    
-    calcRoute(directionsService,directionsDisplay,start,fin);
-}
-
-function calcRoute(directionsService, directionsDisplay,s,f) {
-        directionsService.route({
-          origin: s,  // Haight.
-          destination: f,  // Ocean Beach.
-          // Note that Javascript allows us to access the constant
-          // using square brackets and a string value as its
-          // "property."
-          travelMode: google.maps.TravelMode['WALKING']
-        }, function(response, status) {
+function calcRoute(directionsService, directionsDisplay, route) {
+        directionsService.route(route, function(response, status) {
           if (status == 'OK') {
             directionsDisplay.setDirections(response);
+            save();
           } else {
             window.alert('Directions request failed due to ' + status);
           }
-        });
+        });     
 }
+
+function save(){
+    //waypoints = (directionsDisplay.directions.geocoded_waypoints);
+    var geo = directionsDisplay.directions.geocoded_waypoints;
+    for(i=0; i<geo.length;i++){
+        points[i]={placeId: geo[i].place_id};
+        if(0<i&&i<geo.length-1){
+           points[i]={location: {placeId: geo[i].place_id},stopover:false}; 
+        }
+    }
+    console.log(points);
+    //$('#slongtext').val(start.lng());
+    //$('#slattext').val(start.lat());
+    //$('#flongtext').val(fin.lng());
+    //$('#flattext').val(fin.lat());
+    
+}
+
+function load(){
+    //initMap();
+    //route.origin=waypoints[0].place_id;
+    route.origin=points[0];
+    route.destination=points[points.length-1];
+    if(points.length>2){
+        if(points.length==3){
+          route.waypoints=[points[1]];  
+        }else{
+          route.waypoints=points.slice(1,points.length-1);
+        }
+    }
+    
+    calcRoute(directionsService, directionsDisplay, route);
+}
+
